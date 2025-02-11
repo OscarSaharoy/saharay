36b69cf977695fc42811fb648198bd1cbdc52e2c
Oscar Saharoy <osaharoy@gmail.com>
2025-02-11 10:49:49 +0530

    make common

---
+++ common.js
@@ -0,0 +1,174 @@
+const bodyHTML = `
+    <nav>
+        <img src="/assets/logo.png" style="height: 2rem;" />
+        <div style="line-height: 1.1;">
+            <p> Mr Mrinal Saharay
+            <strong style="font-size: 0.8rem;">Ph.D FRCS</strong>
+            </p>
+            <p style="font-size: 0.8rem; opacity: 0.7;"> Consultant General Surgeon </p>
+        </div>
+        <a href="tel:+447921456892">
+            <img src="/assets/phone-black.svg" style="height: 1.2rem" />
+            <p> Call </p>
+        </a>
+        <button id="menu-button">
+            <img src="/assets/menu.svg" style="height: 1.22rem" />
+            <p> Menu </p>
+        </button>
+    </nav>
+
+    <section id="menu">
+        <div class="options">
+            <p class="em" style="margin-bottom: -1rem;">Menu</p>
+            <a href="/"><p class="heading">Home</p></a>
+            <a href="/services"><p class="heading">Services</p></a>
+            <a href="/consultations"><p class="heading">Consultations</p></a>
+            <a href="/experience"><p class="heading">Experience</p></a>
+            <a href="/contact"><p class="heading">Contact</p></a>
+        </div>
+        <p style="font-size: 0.8rem; max-width: 15rem;"> Mr Saharay has over 30 years experience practicing as a Consultant, serving as Clinical Lead of Thyroid Surgery at Queen's Hospital, Romford, and significantly advancing his chosen sub-specilities, becoming a fellow of the Royal College of Surgeons of England. </p>
+        <div class="links">
+            <p>
+                <img src="/assets/phone.svg" style="height: 1rem;" />
+                <a href="tel:+447921456892">
+                    +44 7921 456892
+                </a>
+            </p>
+            <p>
+                <img src="/assets/mail.svg" style="height: 1rem;" />
+                <a href="mailto:info@saharay.co.uk">
+                    info@saharay.co.uk
+                </a>
+            </p>
+        </div>
+    </section>
+
+    <div id="menu-clickout"></div>
+
+    <div class="pattern"></div>
+
+    <main>
+        <div class="card white">
+            <p class="em">Introduction</p>
+            <p class="heading"> Mr Mrinal Saharay is a Consultant General Surgeon with extensive knowledge in endocrine and colorectal surgery.</p>
+            <p> Mr Saharay has over 30 years experience practicing as a Consultant, serving as Clinical Lead of Thyroid Surgery at Queen's Hospital, Romford, and significantly advancing his chosen sub-specilities, becoming a fellow of the Royal College of Surgeons of England. </p>
+
+            <img src="/assets/portrait.png" style="max-width: 15rem; border-radius: 0.4rem;" />
+
+            <p class="dash"> — </p>
+        </div>
+
+        <p class="em"> Specialism </p>
+        <p class="heading"> Mr Saharay specialises in endocrine and colorectal surgery. </p>
+        <p>Mr Saharay's endocrine experience encompasses all thyroid and parathyroid procedures, including those for cancers as well as minimally invasive parathyroid surgery. In addition, he performs a wide range of colonic operations for both cancers and benign conditions. He has a particular interest in the treatment of haemorrhoids and performs the latest operation for this condition called the Procedure for Prolapse and Haemorrhoids (PPH). In addition he performs gastroscopy and colonoscopy, along with other general operations such as hernia repairs and laparoscopic gall bladder surgery.</p>
+
+        <p class="dash"> — </p>
+
+        <p class="em"> Experience </p>
+        <p class="heading"> Mr Saharay has decades of experience in his field. </p>
+        <p> Mr Saharay’s initial medical training was carried out in India, with his higher surgical training completed in the North East Thames area. As clinical lead for thyroid surgery at Queens Hospital, Romford, and network lead for the North East Thames Cancer Network, he has been responsible for introducing improvements to the service and advancing the sub-specialty considerably. He has worked closely with the endocrinology department at Queens, undertaking joint clinics to provide the best possible care. Mr Saharay is a member of The British Association of Endocrine Surgeons and The British Association of Colorectal Surgeons, taking an active part in their annual conferences.</p>
+
+        <p class="dash"> — </p>
+
+        <p class="em"> Practice </p>
+        <p class="heading"> Mr Saharay practices at Queen's Hospital Romford, and at private hospitals across Essex. </p>
+        <p> Mr Saharay’s NHS practice is at Queen’s Hospital Romford and he is also available for consultation in various private hospitals in Essex. For further information or a private consultation regarding any of the above conditions, please feel free to contact us on 07921456892 or ask your GP for a referral. Mr Saharay is a recognised and accredited fee assured provider for all major insurance companies. </p>
+
+        <p class="dash"> — </p>
+
+        <div class="card">
+            <p class="heading"> Make an Appointment </p>
+            <p> Please contact us at your convenience to book an appointment. Mr Saharay practices at a number of private hospitals across Essex, and also offers online consultations.</p>
+            <div class="links" style="font-size: 1.32rem;">
+                <p>
+                    <img src="/assets/phone.svg" style="height: 1rem;" />
+                    <a href="tel:+447921456892">
+                        +44 7921 456892
+                    </a>
+                </p>
+                <p>
+                    <img src="/assets/mail.svg" style="height: 1rem;" />
+                    <a href="mailto:info@saharay.co.uk">
+                        info@saharay.co.uk
+                    </a>
+                </p>
+            </p>
+        </div>
+
+    </main>
+
+    <section id="buttons">
+        <a href="contact">
+            <img src="/assets/bell.svg" style="height: 1rem; transform: translateY(-.2rem);" />
+            <p> Contact us </p>
+        </a>
+        <a href="tel:+447921456892">
+            <img src="/assets/phone.svg" style="height: 1rem; transform: translateY(-.2rem);" />
+            <p> Call now </p>
+        </a>
+    </section>
+
+    <footer>
+        <section style="line-height: 1.4;">
+            <img src="/assets/logo.png" style="height: 4rem; filter: brightness(8000%);" />
+
+            <p> Dedicated to clinical excellence </p>
+
+            <p class="dash" style="color: white;"> — </p>
+
+            <div style="display: grid; gap: 0.5rem; justify-self: stretch;">
+                <div style="line-height: 1.2;">
+                    <p style="font-size: 0.8rem; opacity: 0.7;"> Consultant General Surgeon </p>
+                    <p> Mr Mrinal Saharay
+                    <strong style="font-size: 0.8rem;">Ph.D FRCS</strong>
+                    </p>
+                </div>
+
+                <div style="line-height: 1.2;">
+                    <p style="font-size: 0.8rem; opacity: 0.7;"> Practice Manager </p>
+                    <p> Bernadette Murray </p>
+                </div>
+                <br>
+                <div class="links">
+                    <p>
+                        <img src="/assets/phone.svg" style="height: 1rem;" />
+                        <a href="tel:+447921456892">
+                            +44 7921 456892
+                        </a>
+                    </p>
+                    <p>
+                        <img src="/assets/mail.svg" style="height: 1rem;" />
+                        <a href="mailto:info@saharay.co.uk">
+                            info@saharay.co.uk
+                        </a>
+                    </p>
+                    <p>
+                        <img src="/assets/map-pin.svg" style="height: 1rem;" />
+                        <span> 10 Lower Bury Lane, Epping, Essex, CM16 5HA </span>
+                    </p>
+                </div>
+                <p style="font-size: 0.8rem;"> <br>© Essex Surgical Practice 2024 </p>
+            </div>
+        </section>
+    </footer>
+`;
+
+document.getElementById("root").innerHTML = bodyHTML;
+
+const menu = document.getElementById("menu");
+const menuButton = document.getElementById("menu-button");
+const menuClickout = document.getElementById("menu-clickout");
+
+menuButton.onclick = () => {
+    menu.classList.toggle("open");
+    menuClickout.classList.toggle("open");
+    menuButton.querySelector("img").src =
+        menu.classList.contains("open") ? "/assets/x.svg" : "/assets/menu.svg";
+};
+menuClickout.onclick = menuButton.onclick;
+
+[...menu.querySelectorAll(".options a")].forEach( el => {
+    if( el.getAttribute("href") === location.pathname ) {
+        el.style.color = "var(--blue)";
+    }
+});
